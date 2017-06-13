309856c86702a6786da15d0a8f4d8c0055712f70
Ben Wheatley <ben@kitsunesoftware.com>
2017-06-13 01:30:00 +0100

    command line arguments, stats

---
+++ stats.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import matplotlib.pyplot as plt
+
+file = open("limerick_x100_iter_loss.txt", "r")
+
+numbers = []
+
+for line in file:
+	try:
+		numbers.append(float(line))
+	except ValueError as e:
+		print e
+
+plt.plot(numbers)
+plt.ylabel('Loss')
+plt.show()
